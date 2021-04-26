c3498371a16e0198effa7cd95a9b3a8bba9a11c4
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-04-26 16:28:51 -0500

    Configuracion de webpack-dev-server

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HTMlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    mode: "production",
+    resolve: {
+        extensions: [".js", ".jsx"],
+        alias: {
+            "@components": path.resolve(__dirname, "src", "components"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HTMlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+    ],
+    devServer: {
+        open: true,
+        contentBase: path.join(__dirname, "dist"),
+        historyApiFallback: true,
+        compress: true,
+        port: 3001,
+    },
+};
