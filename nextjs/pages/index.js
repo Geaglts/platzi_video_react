7da10ef13fbb8fab8f099ed01bab292195b35242
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-05-14 15:22:29 -0500

    Cómo implementar Next.js

---
+++ nextjs/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>Hola Mundo</h1>
+          <p>Este es un ejemplo con NextJS</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
